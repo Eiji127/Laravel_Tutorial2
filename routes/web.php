2910d4978f7e427c173b1768e0559084262f7587
Eiji127 <64912886+Eiji127@users.noreply.github.com>
2022-04-18 11:37:48 +0900

    [Update]: 特定のデータを取得するfindとfindOrFail

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', '\App\Http\Controllers\EntriesController@index');
+
+Route::get('/{id}', '\App\Http\Controllers\EntriesController@view');
